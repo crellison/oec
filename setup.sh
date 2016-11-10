10c0b5187d517b0a0961b5ad89c5e384a35a5255
crellison <cole.ellison@gmail.com>
2016-11-09 20:59:49 -0500

    setup chapter files

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+pugEnd='.pug'
+mdEnd='.md'
+function pugText() {
+  echo "extends ../reveal.pug
+
+block title
+  title OEC Chapter ${1}
+  - var markdownFile = '${1}.md'";
+}
+
+for (( i = 1; i < 33; i++ )); do
+  touch chapters/$i$pugEnd chapters/$i$mdEnd
+  pugText $i >> chapters/$i$pugEnd
+done
